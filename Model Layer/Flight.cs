ca6eb7a6529f66d8823f9a613b179ec6e161b471
Zoltán Fodor <1074252@ucn.dk>
2019-11-11 12:28:42 +0100

    flightmodellayercom

--- Model Layer/Flight.cs
+++ Model Layer/Flight.cs
@@ -2,6 +2,47 @@ namespace DefaultNamespace
 {
     public class Flight
     {
+        private var flightNo;
+        private var model;
+        private var departureTime;
+        private var arrivalTime;
+        private var destination;
+        private var departureLocation;
+
+        public Flight(string flightNo , string model , var departureTime , var arrivalTime , var destination , var departureLocation){
+            this.flightNo = flightNo;
+            this.model = model;
+            this.departureTime = departureTime;
+            this.arrivalTime = arrivalTime;
+            this.destination = destination;
+            this.departureLocation = departureLocation;
+        }
+
+        public var flightNo{
+            get{return flightNo ;}
+            set{this.flightNo = value;}
+        }
+        public var model{
+            get{return model ;}
+            set{this.model = value;}
+        }
+        public var departureTime{
+            get{return departureTime ;}
+            set{this.departureTime = value;}
+        }
+        public var arrivalTime{
+            get{return arrivalTime ;}
+            set{this.arrivalTime = value;}
+        }
+        public var destination{
+            get{return destination ;}
+            set{this.destination = value;}
+        }
+        public var departureLocation{
+            get{return departureLocation ;}
+            set{this.departureLocation = value;}
+        }
+
         
     }
 }
