6d6baebe7cbeb083b51492fda4d1c33fd2ad9606
MII <mnel.jnr@gmail.com>
2019-11-11 12:31:33 +0100

    added a model layer

---
+++ Sem3Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sem3Project
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is a work in progress, the program does not do anything yet");
+        }
+    }
+}
