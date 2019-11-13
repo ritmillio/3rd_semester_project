7d69aa503b4aa2209678a09862fd3761a3a94118
denraskeko <psavag16@student.aau.dk>
2019-11-13 15:09:03 +0100

    Added the real ModelDBIF (I think)

---
+++ AirlineReservations/DatabaseLayer/ModelDBIF.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirlineReservations.Model_Layer;
+
+namespace AirlineReservations.DatabaseLayer
+{
+    public interface ModelDBIF
+    {
+        void InsertModel(Model model);
+        Model GetModel(string modelId);
+        void DeleteModel(string modelId);
+        void updateModel(string modelID, Model model);
+
+    }
+}
