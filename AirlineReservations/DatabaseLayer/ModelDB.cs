8ebdd36116ea77e43a4b31ba296b0f38090d4d7a
denraskeko <psavag16@student.aau.dk>
2019-11-13 15:02:25 +0100

    Added ModelDBIF and ModelDB with a constructor that opens DB connection

---
+++ AirlineReservations/DatabaseLayer/ModelDB.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using AirlineReservations.Model_Layer;
+
+namespace AirlineReservations.DatabaseLayer
+{
+    class ModelDB : ModelDBIF
+    {
+        SqlConnectionStringBuilder conStringBuilder;
+        SqlConnection con;
+
+        public ModelDB()
+        {
+            conStringBuilder.InitialCatalog = "dmaa0918_1071480";
+            conStringBuilder.DataSource = "kraka.ucn.dk";
+            conStringBuilder.UserID = "dmaa0918_1071480";
+            conStringBuilder.Password = "Password1!";
+            con = new SqlConnection(conStringBuilder.ConnectionString);
+        }
+
+        public void DeleteModel(string modelId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Model GetModel(string modelId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertModel(Model model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateModel(string modelID, Model model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PlaneModelDBIF.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.SqlClient;
+using Model_Layer;
+
+namespace Database_Layer
+{
+    public interface PlaneModelDBIF
+    {
+        public void insertVoid(Model planeModel);
+
+    }
+
+}
+
