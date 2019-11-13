b6a283b473ea9c3f8613b03ac3182d0d69b41422
denraskeko <psavag16@student.aau.dk>
2019-11-13 19:04:23 +0100

    Added all other DBIF classes for the model layer classes we currently have

---
+++ AirlineReservations/DatabaseLayer/ReservationDBIF.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirlineReservations.Model_Layer;
+
+namespace AirlineReservations.DatabaseLayer
+{
+    interface Reservation
+    {
+        void InsertReservation(Reservation reservation);
+        Reservation GetReservationById(int bookingNo);
+        void DeleteReservation(int bookingNo);
+        void UpdateReservation(int bookingNo, Reservation reservation);
+        List<Reservation> GetAllReservations();
+
+    }
+}
