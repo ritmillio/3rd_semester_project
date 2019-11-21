a16b7a0eee6206259e461f59dc56cbb1c1a8d585
Zoltan Fodor <1074252@ucn.dk>
2019-11-21 17:39:31 +0100

    html_fixes

--- Gui/Web/js/main.js
+++ Gui/Web/js/main.js
@@ -7,6 +7,16 @@ else{
     console.log("Somethings wrong");
 }
 --> jquery loaded */
+let button;
+let from;
+let to;
 
+button = document.getElementById("searchbutton");
+from = document.getElementById("fromsearchinput");
+to = document.getElementById("tosearchinput");
+
+function search(){
+    window.location("seatreservation.html");
+}
 
 
