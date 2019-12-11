e86f99939899d0d65bd55b66459b5c06591c7d25
denraskeko <psavag16@student.aau.dk>
2019-12-11 10:13:39 +0100

    Added webGUI so far

---
+++ webgu/webgu/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace webgu.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        ServiceReference1.Flight_ControllerServiceIFClient proxy;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            proxy = new ServiceReference1.Flight_ControllerServiceIFClient();
+        }
+
+    }
+}
---
+++ webgu/webgu/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+var button = document.getElementById("buttonsubmit");
+
+function showFlights() {
+    window.location.href("showflights.cshtml");
+}
+window.onlick = showFlights;
